83656d273878e30c94e730bbc796c0c8f90c222d
Sameh ElGazar <selgazar95@gmail.com>
2023-12-16 04:46:08 +0200

    Task 0--1

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,9 +15,9 @@ void bubble_sort(int *array, size_t size)
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size; j++)
+		for (j = 0; j < size - 1; j++)
 		{
-			if (array[j] > array[j + 1] && array[j + 1])
+			if (array[j + 1] && array[j] > array[j + 1])
 			{
 				swap(&array[j], &array[j + 1]);
 				print_array(array, size);
