149de7540404af81495f8f7d053d4ec2dc1b2e67
Sameh ElGazar <selgazar95@gmail.com>
2023-12-16 05:18:14 +0200

    Task 0--1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,24 @@
 #include "sort.h"
 
+void insertion_sort(int *arr, int size)
+{
+    int i, j, key;
+
+    for (i = 1; i < size; i++)
+    {
+        j = i - 1;
+        key = arr[i];
+
+        while(j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
 void insertion_sort_list(listint_t **list)
 {
-
+    
 }
