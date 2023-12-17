5b742ef13eca54784f2f7902ccabc2f4aa007ac4
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2023-12-17 08:12:49 -0800

    first task

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,5 +7,22 @@
 */
 void bubble_sort(int *array, size_t size)
 {
+	int i, j;
 
+	if (!array)
+	{
+		return;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(array[j], array[j + 1]);
+				print_array(array, size);
+			}
+		}
+	}
 }
---
+++ swap.c
@@ -0,0 +1,15 @@
+#include "sort.h"
+
+/**
+ *swap - this is the function
+ *@f: this is a vraiable
+ *@s: this is a vraiable
+ *Return: return 0
+*/
+void swap(int *f , int *s)
+{
+	int temp = f;
+
+	f = s;
+	s = temp;
+}
