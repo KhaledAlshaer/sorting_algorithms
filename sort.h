1c6530b3127424f89ced8e55e7b52fe93ef30eca
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2023-12-17 08:33:12 -0800

    first task

--- sort.h
+++ sort.h
@@ -13,12 +13,12 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
-void swap(int *f , int *s);
+void swap(int *f, int *s);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
